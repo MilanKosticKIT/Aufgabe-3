aaa811576843cbf91459cd0841fac38853581b70
Milan_K <allgemein@milan-kostic.de>
2017-11-15 16:25:51 +0100

    Initial Commit

---
+++ Aufgabe 3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Aufgabe 3
+//
+//  Created by Milan Kostic on 15.11.17.
+//  Copyright © 2017 Milan Kostic. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
