52c980e5c730373b8b85ac174fea94803d5b2c99
Milan_K <allgemein@milan-kostic.de>
2017-11-25 18:56:01 +0100

    Adding scope for ending of word--> correct checking whether going on with algorithms. Also formatted.

--- Aufgabe 3/main.c
+++ Aufgabe 3/main.c
@@ -3,36 +3,44 @@
 
 int breakIntoWords(char* line, int maxWords, char* words[]);
 int jumpToEndOfWord(char** actualPosition);
+int jumpToStart(int* going,char** line);
 
 //MARK: - Working Code -
 int breakIntoWords(char* line, int maxWords, char* words[]){
     int wordCounter;
-    for(wordCounter = 0; wordCounter < maxWords; wordCounter++) {
-        char* wordStart = line;
-        int wordLength = jumpToEndOfWord(&line);
-        //TODO: Check whether line is at its end or wehter is just the words end.
-        char* endOfword = line;
-        char* wordLocation = (char*)malloc(sizeof(char) * (wordLength + 1));
-        
-        //Mark word end.
-        *(wordStart + wordLength) = '\0';
-        //USEFULL when i store it osomewhere else?
-        //Possibility to leave the check for end of line into for-statement
-        
-        words[wordCounter] = wordLocation;
-        
-        //Write word into new location.
-        for (int writingCounter = 0; writingCounter < wordLength; writingCounter++) {
-            *wordLocation = *line;
-            wordLocation++;
-            line++;
+    int goinig = 1; //Wheter the algoriythm should go on.
+    for(wordCounter = 0; wordCounter < maxWords && goinig == 1; wordCounter++) {
+        if(jumpToStart(&goinig, &line)){
+            char* wordStart = line;
+            int wordLength = jumpToEndOfWord(&line);
+            char* endOfword = line;
+            char* wordLocation = (char*)malloc(sizeof(char) * (wordLength + 1));
+            
+            //Mark word end.
+            *(wordStart + wordLength) = '\0';
+            //USEFULL when i store it osomewhere else?
+            //Possibility to leave the check for end of line into for-statement
+            
+            words[wordCounter] = wordLocation;
+            
+            //Write word into new location.
+            for (int writingCounter = 0; writingCounter < wordLength; writingCounter++) {
+                *wordLocation = *line;
+                wordLocation++;
+                line++;
+            }
+            *wordLocation = '\0';
+            
+            //Checking whether the line is at its end.
+            if (*line == '\0') {
+                goinig = 0;
+            } else {
+//                *line = '\0';
+                //Not Neccessary when using malloc
+                line++;
+            }
         }
-        *wordLocation = '\0';
-        //TODO: Think about advancing line.
-        //TODO: Check whether line is at its end or wehter is just the words end.
     }
-    
-    
     return wordCounter;
 }
 
@@ -44,12 +52,22 @@ int jumpToEndOfWord(char** actualPostion){
     return counter;
 }
 
+int jumpToStart(int* going,char** line){
+    //Jumps over all ' ' that are at the begininig of the current (possible)
+    for(;**line == ' ' && **line != '\0';*line++){}
+    if (**line != '\0') {
+        *going = 1;
+    } else {
+        *going = 0;
+    }
+    return *going;
+}
 
 //MARK: - Testing Area -
 typedef struct{
-char* line;
-int actualWords;
-int maxWords;
+    char* line;
+    int actualWords;
+    int maxWords;
 }TestCase;
 
 
